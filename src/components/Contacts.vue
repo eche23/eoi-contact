576bafc5463f7c7a29e8e41532cb8cc4658c0d67
eche23 <eche2312@gmail.com>
2019-08-13 16:24:43 +0100

    full funtionality

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -3,10 +3,36 @@
     <input type="text" id="name" v-model="name">
     <input type="text" id="email" v-model="email">
     <button v-on:click="addContact">ADD</button>
-    <div id="contacts">
-      <div class="contact" v-for="contact in contacts" v-bind:key="contact.email">
-        <h2>{{contact.name}} {{contact.email}} {{contact.contacted}}</h2>
+    <br>
+    <button v-on:click="deletedAll">Delete ALL</button>
+    <button v-on:click="allContacted">Contacted ALL</button>
+    <button v-on:click="showOnlyContacted">Show Only Contacted</button>
 
+    <div id="Allcontacts" v-if="!onlyContacted">
+      <div class="contact" v-for="contact in contacts" v-bind:key="contact.email">
+        <div class="contactedTrue" v-if="contact.contacted">
+          <h2>Name: {{contact.name}} Email: {{contact.email}} Contacted: {{contact.contacted}}</h2>
+          <button v-on:click="editContact(contact.email)">Edit</button>
+          <button v-on:click="deletedContact(contact.email)">Delete</button>
+          <input v-on:click="contacted(contact.email)" type="checkbox" checked>
+        </div>
+        <div class="contactedFalse" v-if="!contact.contacted">
+          <h2>Name: {{contact.name}} Email: {{contact.email}} Contacted: {{contact.contacted}}</h2>
+          <button v-on:click="editContact(contact.email)">Edit</button>
+          <button v-on:click="deletedContact(contact.email)">Delete</button>
+          <input v-on:click="contacted(contact.email)" type="checkbox">
+        </div>
+
+      </div>
+    </div>
+    <div id="Onlycontacts" v-if="onlyContacted">
+      <div class="contact" v-for="contact in contacts" v-bind:key="contact.email">
+        <div class="" v-if="contact.contacted">
+          <h2>Name: {{contact.name}} Email: {{contact.email}} Contacted: {{contact.contacted}}</h2>
+          <button v-on:click="editContact(contact.email)">Edit</button>
+          <button v-on:click="deletedContact(contact.email)">Delete</button>
+          <input v-on:click="contacted(contact.email)" type="checkbox" checked>
+        </div>
       </div>
     </div>
 
@@ -21,7 +47,8 @@ export default {
   },
   data(){
     return{
-      contacts: []
+      contacts: [],
+      onlyContacted: false
     }
     
   },
@@ -29,14 +56,54 @@ export default {
     addContact() {
       const cont = this.contacts.find(cont => cont.email === this.email);
       if(typeof cont == "undefined"){
-        const contact = {};
-        contact.name = this.name;
-        contact.email = this.email;
-        contact.contacted = false;
-        this.contacts.push(contact);
+        console.log(this.name.length);
+        if(this.name.length >= 6){
+
+          const contact = {};
+          contact.name = this.name;
+          contact.email = this.email;
+          contact.contacted = false;
+          return this.contacts.push(contact);
+        }else{
+          return alert("The name must have more than 6 letters");
+        }  
       }else{
-        alert("Email repetido");
+        return alert("Email repeated");
       }
+    },
+    editContact(email){
+      const contact = this.contacts.find(cont => cont.email === email);
+      const repeatedContact = this.contacts.find(cont => cont.email === this.email);
+      if(typeof repeatedContact != 'undefined'){
+        contact.email = this.email;
+        contact.name = this.name;
+        return contact;
+      }else{
+        return alert("Email repeated");
+      }
+
+    },
+    deletedContact(email){
+      const index = this.contacts.findIndex(cont => cont.email === email);
+      this.contacts.splice(index, 1);
+    },
+    contacted(email){
+      const contact = this.contacts.find(cont => cont.email === email);
+      contact.contacted = !contact.contacted;
+    },
+    showOnlyContacted(){
+      console.log(this.onlyContacted);
+      
+      this.onlyContacted = !this.onlyContacted;
+    },
+    deletedAll(){
+      this.contacts = [];
+    },
+    allContacted(){
+      this.contacts.forEach(cont => {
+        cont.contacted = true;
+      });
+      
     }
   },
 }
